48ec723a375d931b096d81de1348e03289223347
wasimmahamod <wasimmahamodraza@gmail.com>
2024-06-09 19:43:34 +0600

    crud Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { getDatabase, ref, set, push, onValue } from "firebase/database";
+import { getDatabase, ref, set, push, onValue , remove, update} from "firebase/database";
 
 const App = () => {
+
+  const db = getDatabase();
+  
+  let [editModal, setEditModal]=useState(false)
   let [task, setTask] = useState("");
   let [alltodos, setAlltodos] = useState([]);
+  let [updatedTask ,setUpdatedTask]=useState('')
+  let [id, setId]=useState('')
   let handleTask = (e) => {
     setTask(e.target.value);
   };
 
+
+
+
   let handleSubmit = () => {
-    const db = getDatabase();
+
     set(push(ref(db, "todos/")), {
       name: task,
     }).then(() => {
@@ -18,19 +27,42 @@ const App = () => {
     });
   };
 
+
   useEffect(() => {
-    const db = getDatabase();
+
     const todosRef = ref(db, "todos/");
     onValue(todosRef, (snapshot) => {
       let array = [];
       snapshot.forEach((item) => {
-        array.push(item.val());
+        array.push({...item.val(), key:item.key});
       });
       setAlltodos(array);
     });
   }, []);
 
-  console.log(alltodos);
+function handleDelete(id){
+
+  remove(ref(db, "todos/" + id))
+}
+
+
+let handleEditModal=(id)=>{
+  setId(id)
+  setEditModal(true)
+}
+
+let handleUpdateTask=(e)=>{
+  setUpdatedTask(e.target.value)
+}
+
+let handleUpdate=()=>{
+
+  update(ref(db, "todos/" + id),{
+    name:updatedTask
+  }).then(()=>{
+    setEditModal(false)
+  })
+}
   return (
     <div className=" mx-auto w-[400px] text-center ">
       <h1>Add todo </h1>
@@ -50,8 +82,18 @@ const App = () => {
       </button>
       <ul>
         {alltodos.map((item) => {
-          return <li>{item.name}</li>;
+          return <li className="my-3">{item.name} <button onClick={()=>handleDelete(item.key)} className=" bg-red-500 text-white px-3 rounded-full ">X</button> <button onClick={()=>handleEditModal(item.key)} className=" bg-blue-500 text-white px-3 rounded-full ">Edit</button></li>;
         })}
+  {editModal && 
+        <div className=" w-[400px]  py-10 absolute top-0 left-[50%] translate-x-[-50%] bg-gray-500  rounded-md">
+
+          <input onChange={handleUpdateTask} type="text"  placeholder="Update your task"/>
+
+          <button onClick={handleUpdate}  className=" bg-green-500   text-white">Update</button>
+          <button onClick={()=>setEditModal(false)} className=" absolute top-2 right-2 bg-red-500 p-2 rounded-full">X</button>
+        </div>
+  
+  }
       </ul>
     </div>
   );
